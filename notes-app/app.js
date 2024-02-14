00024d49e9acfb18e1fa86eae9705be2d344e5a5
Prakhar Verma <prakharverma1022@gmail.com>
2024-02-14 17:29:56 +0530

    S4-L4 argument parsing with yargs part-2

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,21 @@ const getNotes = require('./notes.js');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note!');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
     }
 })
 
@@ -41,4 +54,7 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv);
+// console.log(yargs.argv);
+// alternatively, use yargs.parse() to parse the arguments
+
+yargs.parse();
