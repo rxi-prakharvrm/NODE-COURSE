2541b5513e8a0c78c365799964ab1dbe00193135
Prakhar Verma <prakharverma1022@gmail.com>
2024-02-14 15:51:36 +0530

    S3-L4 importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,17 @@
-const getNotes = require('./notes.js');
+const validator = require('validator');
+// const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// const msg = getNotes();
+// console.log(msg);
 
-console.log(msg);
+// correct email
+console.log(validator.isEmail("foo@gmail.com"));
+
+// incorrect email
+console.log(validator.isEmail("foo@com"));
+
+// correct url
+console.log(validator.isURL("https://www.google.com"));
+
+// incorrect url
+console.log(validator.isURL("ht://www.google.com"));
