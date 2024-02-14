4e12622f2305dcfea8db6c4720490e7375843c42
Prakhar Verma <prakharverma1022@gmail.com>
2024-02-14 16:53:58 +0530

    OWN: practice with chalk npm module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,23 @@
-// install nodemon globally
-// npm install nodemon -g
-// nodemon app.js instead of node app.js in terminal
-
 const chalk = require('chalk');
 
-console.log(chalk.green("Success!"));
-console.log(chalk.red("Access Denied!\n"));
+const msg = 'Hello world';
 
-console.log(chalk.bold.green("Success!"));
-console.log(chalk.bold.red("Access Denied!\n"));
+// applying styles
+console.log(`Color: ${chalk.blue(msg)}`);
+console.log(`Background Color: ${chalk.bgRed(msg)}`);
+console.log(`Underline: ${chalk.underline(msg)}`);
+console.log(`Bold: ${chalk.bold(msg)}`);
+console.log(`Inverse: ${chalk.inverse(msg)}`);
+console.log(`Color: ${chalk.hex('#DEADED').bold(msg)}`);
+console.log(`Color: ${chalk.rgb(123, 45, 67).bold(msg)}`);
+console.log(`Color: ${chalk.hsl(123, 45, 67).bold(msg)}`);
+console.log(`Color: ${chalk.keyword('orange').bold(msg)}`);
 
-console.log(chalk.bold.green.inverse("Success!"));
-console.log(chalk.bold.red.inverse("Access Denied!\n"));
+// chaining styles
+console.log(chalk.hex('#FFFFFF').bold.inverse('Hello world!'));
 
-console.log(chalk.green.inverse("Success!"));
-console.log(chalk.red.inverse("Access Denied!\n"));
+// others (in latest versions of the chalk not in @2.4.1)
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
+// console.log(info('Info!'));
+// console.log(success('Success!'));
