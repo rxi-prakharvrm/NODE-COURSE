971e24f4b6570ec3a715f2468aae0d6b532042c7
Prakhar Verma <prakharverma1022@gmail.com>
2024-02-14 16:39:19 +0530

    S3-L6 global npm modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,17 @@
-// const getNotes = require('./notes.js');
-
-// const msg = getNotes();
+// install nodemon globally
+// npm install nodemon -g
+// nodemon app.js instead of node app.js in terminal
 
 const chalk = require('chalk');
 
-console.log(chalk.green("Success!\n"));
+console.log(chalk.green("Success!"));
 console.log(chalk.red("Access Denied!\n"));
 
-console.log(chalk.bold.green("Success!\n"));
+console.log(chalk.bold.green("Success!"));
 console.log(chalk.bold.red("Access Denied!\n"));
 
-console.log(chalk.bold.green.inverse("Success!\n"));
+console.log(chalk.bold.green.inverse("Success!"));
 console.log(chalk.bold.red.inverse("Access Denied!\n"));
 
-console.log(chalk.green.inverse("Success!\n"));
+console.log(chalk.green.inverse("Success!"));
 console.log(chalk.red.inverse("Access Denied!\n"));
